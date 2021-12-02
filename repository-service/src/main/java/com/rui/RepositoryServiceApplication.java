91b8fcfc754bbe26b62752bdac3ddaf3344d480f
rui <ooruilio@gmail.com>
2021-12-02 15:39:52 +0800

    create module: base-service & common-service & repository-service

---
+++ repository-service/src/main/java/com/rui/RepositoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryServiceApplication.class, args);
+    }
+
+}
---
+++ repository-service/src/test/java/com/rui/RepositoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
