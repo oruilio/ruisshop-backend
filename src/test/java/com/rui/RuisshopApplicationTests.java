58031687cc9ba32ce9cc3dce47bfd67d9559a295
rui <ooruilio@gmail.com>
2021-12-02 15:31:53 +0800

    build parent environment

---
+++ src/test/java/com/rui/RuisshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuisshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
