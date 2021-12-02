d03faa70a65a878db0c6b7ea1cff635fde69b21f
rui <ooruilio@gmail.com>
2021-12-02 16:27:09 +0800

    common-service: add SwaggerConfiguration

---
+++ common-service/src/main/java/com/rui/configuration/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package com.rui.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.rui"))
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("RUI优选系统测试接口")
+                        .description("Product商品管理")
+                        .version("V1.0")
+                        .build());
+    }
+}
